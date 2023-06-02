d02009648c2f7b4c9ecd9dcde7d06737ff4f6577
= <mayuresh9823465901@gmail.com>
2023-06-02 23:46:13 +0530

    adf

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,6 +6,10 @@ from .import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-    # Home page 
-    path('' , views.index , name = 'index'),
+    # Home page
+    path('',  views.index, name='index'),
+    # Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic')
 ]
